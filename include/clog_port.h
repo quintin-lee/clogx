4883d9e966c047646a7181e38a8a2d2fe89de7e3
quintin <2449164582@qq.com>
2026-08-05 07:39:21 +0800

    fix(prometheus): 🐛 ensure g_prom_mutex is dynamically initialized before use
    Initialize g_prom_mutex via clog_mutex_init in clog_prometheus_exporter_start to ensure TSan registers the mutex object before concurrent access on macOS.

--- include/clog_port.h
+++ include/clog_port.h
@@ -266,19 +266,15 @@ static inline struct tm *clog_gmtime_r(const time_t *timep, struct tm *result)
 #if (defined(__SANITIZE_THREAD__) || (defined(__clang__) && __has_feature(thread_sanitizer))) &&   \
     defined(__APPLE__)
 #include <sanitizer/tsan_interface.h>
-#define CLOG_TSAN_MUTEX_CREATE(m) __tsan_mutex_create((m), 0)
-#define CLOG_TSAN_MUTEX_DESTROY(m) __tsan_mutex_destroy((m), 0)
-#define CLOG_TSAN_PRE_LOCK(m) __tsan_mutex_pre_lock((m), 0)
-#define CLOG_TSAN_POST_LOCK(m) __tsan_mutex_post_lock((m), 0, 0)
-#define CLOG_TSAN_PRE_UNLOCK(m) __tsan_mutex_pre_unlock((m), 0)
-#define CLOG_TSAN_POST_UNLOCK(m) __tsan_mutex_post_unlock((m), 0)
+#define CLOG_TSAN_MUTEX_CREATE(m) __tsan_mutex_create((m), __tsan_mutex_not_static)
+#define CLOG_TSAN_MUTEX_DESTROY(m) __tsan_mutex_destroy((m), __tsan_mutex_not_static)
+#define CLOG_TSAN_ACQUIRE(m) __tsan_acquire(m)
+#define CLOG_TSAN_RELEASE(m) __tsan_release(m)
 #else
 #define CLOG_TSAN_MUTEX_CREATE(m)
 #define CLOG_TSAN_MUTEX_DESTROY(m)
-#define CLOG_TSAN_PRE_LOCK(m)
-#define CLOG_TSAN_POST_LOCK(m)
-#define CLOG_TSAN_PRE_UNLOCK(m)
-#define CLOG_TSAN_POST_UNLOCK(m)
+#define CLOG_TSAN_ACQUIRE(m)
+#define CLOG_TSAN_RELEASE(m)
 #endif
 
 typedef struct stat clog_stat_t;
@@ -322,16 +318,13 @@ static inline void clog_mutex_destroy(clog_mutex_t *m)
 }
 static inline void clog_mutex_lock(clog_mutex_t *m)
 {
-    CLOG_TSAN_MUTEX_CREATE(m);
-    CLOG_TSAN_PRE_LOCK(m);
     pthread_mutex_lock(m);
-    CLOG_TSAN_POST_LOCK(m);
+    CLOG_TSAN_ACQUIRE(m);
 }
 static inline void clog_mutex_unlock(clog_mutex_t *m)
 {
-    CLOG_TSAN_PRE_UNLOCK(m);
+    CLOG_TSAN_RELEASE(m);
     pthread_mutex_unlock(m);
-    CLOG_TSAN_POST_UNLOCK(m);
 }
 
 /* ── RWLock via pthread_rwlock_t ── */
